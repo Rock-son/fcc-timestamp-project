0095bf0af139f750b521f30c1e968a9551b29af6
rok <roky.zz@gmail.com>
2018-10-02 13:37:46 +0200

    finished api/timestamp route handler

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +19,28 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:date_string?", function(req, res) {
 
+    var dateParam = req.params.date_string || new Date(),
+        options = {year: 'numeric', month: 'long', day: 'numeric'}; // obsolete - valid for "new Date().toLocaleDateString("en-us", options)"
+
+    res.set({status: 200, 'content-type': 'text/plain' });
+
+    if(new Date().setSeconds(+dateParam)) {
+        res.send(JSON.stringify({unix: dateParam, natural: new Date(+dateParam * 1000).toUTCString()}));
+    } else if (Date.parse(dateParam)) {
+        res.send(JSON.stringify({unix: Date.parse(dateParam) / 1000, natural: new Date(dateParam).toUTCString()}));
+    } else {
+        res.send(JSON.stringify({unix: null, natural: "Invalid Date"}));
+    }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
